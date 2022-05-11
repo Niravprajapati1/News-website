956332586eece8ef4e17857bbbfbf6a1d9b91c46
Niravprajapati1 <niravprajapati97120@gmail.com>
2022-05-11 19:53:18 +0530

    New news api

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 
-  const url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ae8ac7611e9c44ac896a3309adf84180"
+  const url = "https://api.thenewsapi.com/v1/news/top?api_token=whIfWwb6ycjYjsEQSlCMg8iz6PwvkasigcltwF7L&locale=in&limit=5"
   ;
-  const req = new Request(url)
- let ne;
-  fetch(req)
-    .then(req => req.json())
-    .then(result => news(result.articles))
-document.addEventListener('load',()=>{
-  news();
-})
+ 
+ const url2 = "https://api.thenewsapi.com/v1/news/all?api_token=whIfWwb6ycjYjsEQSlCMg8iz6PwvkasigcltwF7L&language=en&categories=business%2Ctech&exclude_categories=travel&published_after=2022-05-04";
+  fetch(url)
+    .then(url => url.json())
+    .then(result => news(result.data))
+fetch(url2)
+  .then(url2 => url2.json())
+  .then(result => news(result.data))
 let news = articles => {
   
   
 articles.forEach(article =>{
-  ne = article.title;
+ 
 let ArticlesDesc = document.createElement('div');
 let cards = document.getElementById('cards').appendChild(ArticlesDesc);
     ArticlesDesc.innerHTML = `<div id="allCards">
     <div class="card" style="width: 30rem;">
-      <img src="${article.urlToImage}" class="card-img-top" id = "newsimage" alt="internet connection error" height="100px" width="120px">
+      <img src="${article.image_url}" class="card-img-top" id = "newsimage" alt="internet connection error" height="100px" width="120px">
       <div class="card-body">
         <h5 class="card-title">${article.title}</h5>
         <p class="card-text">${article.description}</p>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">author : ${article.author}</li>
-        <li class="list-group-item">publish at ${article.publishedAt}</li>
+        <li class="list-group-item">source : ${article.source}</li>
+        <li class="list-group-item">publish at ${article.published_at}</li>
         
       </ul>
       <a href="${article.url}" class="btn btn-primary">show more</a>
