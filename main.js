f7db8332e0f0ccc6cab3ada52604acb58d538f1b
Nirav <niravprajapati97120@gmail.com>
2022-06-13 11:24:47 +0530

    Project completed

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+/*let burger = document.getElementById('burger');
+let navlinks = document.getElementById('nav-links')
+  
+burger.addEventListener('click',()=>{
+   navlinks.classList.toggle('is-active');
+   burger.classList.toggle('is-active')
+ });
+  */
+  document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
+
 
   const url = "https://api.thenewsapi.com/v1/news/top?api_token=whIfWwb6ycjYjsEQSlCMg8iz6PwvkasigcltwF7L&locale=in&limit=5"
   ;
