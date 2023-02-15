d7f9097b0b6ed9d70f2f209ae01b172842849b5a
Nirav_Prajapati <niravprajapati97120@gmail.com>
2023-02-15 00:51:15 +0000

    added responsiveness in feed.js

--- components/feed.js
+++ components/feed.js
@@ -1,35 +1,33 @@
 import { Image, Card, Heading, Text, Stack, Divider, Button, ButtonGroup, CardBody, CardFooter } from '@chakra-ui/react'
+import styles from '../styles/Feed.module.css'
 
 export function Feed({article}) {
   return (    
 
-<Card maxW='sm'>
-  <CardBody>
+<Card maxW='sm' >
+  <CardBody className={styles.boxbackground}>
     <Image
       src={article.image_url} alt='Green double couch with wooden legs'
       borderRadius='lg'
     />
     <Stack mt='6' spacing='3'>
-      <Heading size='md'>{article.title}</Heading>
-      <Text>
+      <Heading fontSize={{ base: '12px', md: '27px', lg: '55px' }}>{article.title}</Heading>
+      <Text fontSize={{ base: '11.6px', md: '25px', lg: '53px' }}>
     {article.description}
       </Text>
-      <Text color='blue.600' fontSize='2xl'>
+      <Text color={'lightblue'} fontSize={{ base: '11.2px', md: '23.5px', lg: '52.5px' }}>
       
     {article.source}
       </Text>
     </Stack>
   </CardBody>
   <Divider />
-  <CardFooter>
-    <ButtonGroup spacing='2'>
-      <Button variant='solid' colorScheme='blue'>
-       <a href={article.url}>
+  <CardFooter background={'blackAlpha.800'}>
+    <ButtonGroup spacing='1' >
+      <Button px='5.2' py='2' variant='solid' color={'darkslateblue'} fontSize={{ base:'10.5px', md: '23px', lg: '51px'}}>
+       <a href={article.url} >
         Read more
         </a>
-      </Button>
-      <Button variant='ghost' colorScheme='blue'>
-    
       </Button>
     </ButtonGroup>
  
