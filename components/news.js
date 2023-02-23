d479dc7bcc1a0f5003023ef5a847ba9aefaef4eb
Nirav_Prajapati <niravprajapati97120@gmail.com>
2023-02-23 10:00:16 +0000

    added new logo

--- components/news.js
+++ components/news.js
@@ -5,12 +5,12 @@ import { Feed } from './feed'
 function News({data}) {
   return (
   
-    <SimpleGrid Center mt={5} minChildWidth={{base: '143px',md: '300px', lg: '405px'}} spacing={7}>
+    <SimpleGrid Center mt={5} minChildWidth={{base: '152px',md: '315px', lg: '405px'}} spacing={7}>
   {
           data.articles.map(((article) => {
             return (
 
-        <Box key={article.uuid}>
+        <Box key={article.uuid} p={5}>
             <Feed article={article} />
         </Box>
             )
